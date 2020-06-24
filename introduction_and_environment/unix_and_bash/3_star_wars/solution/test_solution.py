4010558a37e83f9bda5bbb3cf9451ae556c0ae46
prasida-newman <67298842+prasida-newman@users.noreply.github.com>
2020-06-23 21:16:03 -0400

    Update test_solution.py

--- introduction_and_environment/unix_and_bash/3_star_wars/solution/test_solution.py
+++ introduction_and_environment/unix_and_bash/3_star_wars/solution/test_solution.py
@@ -2,16 +2,16 @@ import os
 
 def test_solution():
     solution = {}
-    for dir_name, subdir_list, file_list in os.walk("3_star_wars"):
+    for dir_name, subdir_list, file_list in os.walk("star_wars"):
     # dir_name, subdir_list, file_list = os.walk(".")
         print(dir_name, subdir_list, file_list)
         solution[dir_name] = file_list
 
-    assert solution == {'3_star_wars': [], 
-                        '3_star_wars/tatooine': [], 
-                        '3_star_wars/tie_fighter': ['darth_vader.txt'], 
-                        '3_star_wars/x_wing': ['luke_skywalker.txt'], 
-                        '3_star_wars/millenium_falcon': ['han_solo.txt', 'chewbacca.txt'], 
-                        '3_star_wars/the_rebellion': ['leia_organa.txt'], 
-                        '3_star_wars/the_empire': []
+    assert solution == {'star_wars': [], 
+                        'star_wars/tatooine': [], 
+                        'star_wars/tie_fighter': ['darth_vader.txt'], 
+                        'star_wars/x_wing': ['luke_skywalker.txt'], 
+                        'star_wars/millenium_falcon': ['han_solo.txt', 'chewbacca.txt'], 
+                        'star_wars/the_rebellion': ['leia_organa.txt'], 
+                        'star_wars/the_empire': []
                         }
