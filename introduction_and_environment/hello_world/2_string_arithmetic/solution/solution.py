15d2352cccb534cd05349a3a8034447f6ef77456
Prasida Newman <prasidan@Prasidas-Mac-mini.local>
2020-06-26 18:08:41 -0400

    adding solutions

--- introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
@@ -1,1 +1,44 @@
 # Code your solution here
+cheese = 'string'
+please = 'string'
+counter = 0
+
+try:
+    print(cheese - please)
+except:
+    counter +=1
+try:
+    cheese + please
+except:
+    counter +=1
+try:
+    cheese + 1
+except:
+    counter +=1
+try:
+    cheese * please
+except:
+    counter +=1
+try:
+    cheese * 2
+except:
+    counter +=1
+try:
+    cheese / please
+except:
+    counter +=1
+try: 
+    cheese ** please
+except:
+    counter +=1
+try:
+    cheese // please
+except:
+    counter +=1
+try:
+    cheese % please
+except:
+    counter +=1
+    
+STR_ARITHMETIC = 9 - counter
+print(STR_ARITHMETIC)
